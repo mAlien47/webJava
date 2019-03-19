ce0dc92896b2b50c537e6573b89ce15788cc6eaf
Mislav Teskera <mislav.teskera@gmail.com>
2019-03-19 21:53:05 +0100

    fragments

--- src/main/java/hr/java/web/teskera/controllers/ExpenseController.java
+++ src/main/java/hr/java/web/teskera/controllers/ExpenseController.java
@@ -1,7 +1,5 @@
 package hr.java.web.teskera.controllers;
 
-import java.io.Serializable;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import hr.java.web.teskera.models.Expense;
 
 @Controller
-@RequestMapping("/expense")
-public class ExpenseController implements Serializable {
+@RequestMapping("/troskovi")
+public class ExpenseController {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -8366543859848255803L;
 
-	@GetMapping("/new")
+	@GetMapping("/novo")
 	public String newExpense(Model model) {
 		model.addAttribute("expense", new Expense());
 		model.addAttribute("expenseType", Expense.ExpenseType.values());
--- src/main/java/hr/java/web/teskera/controllers/HomeController.java
+++ src/main/java/hr/java/web/teskera/controllers/HomeController.java
@@ -1,21 +1,12 @@
 package hr.java.web.teskera.controllers;
 
-import java.io.Serializable;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class HomeController implements Serializable {
+public class HomeController {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2326851147704560552L;
-
-	@GetMapping("/")
 	public String showHome() {
 		return "index";
 	}
